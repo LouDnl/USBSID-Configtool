d4921b99423be7f99812a332dad42aeabcd136e6
loudnl <LouDnl@users.noreply.github.com>
2026-06-04 17:46:35 +0200

    state.clj

---
+++ src/usbsid/state.clj
@@ -0,0 +1,48 @@
+(ns usbsid.state
+  "Fluid stuff"
+  (:require
+   [clojure.string :refer [trim-newline]]
+   [usbsid.config-model :as model]
+   [usbsid.logging :refer [logger]]))
+
+
+(def initial-state
+  {:connection     {:status      :disconnected
+                    :fw-version  nil
+                    :pcb-version nil}
+   :config         model/initial-config
+   :active-section :about
+   :log            [(format
+                     "USBSID-Pico Config Tool v%s"
+                     (trim-newline (slurp ".version")))]
+   :dirty          false})
+
+(defonce *state (atom initial-state))
+
+(defn log!
+  "Logging wrapper"
+  [msg]
+  (swap! *state update :log conj (str "> " msg))
+  (.info @logger (str "> " msg)))
+
+(defn set-section!
+  "Active section changer"
+  [section]
+  (swap! *state assoc :active-section section))
+
+(defn set-config-value!
+  "Change a config value"
+  [path value]
+  (swap! *state
+         #(-> %
+              (assoc-in (into [:config] path) value)
+              (assoc :dirty true))))
+
+(defn set-connection!
+  "Update connection status"
+  [status fw pcb]
+  (swap! *state
+         assoc :connection
+         {:status      status
+          :fw-version  fw
+          :pcb-version pcb}))
