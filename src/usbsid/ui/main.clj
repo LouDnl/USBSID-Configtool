8a5f0fa94085292d338c57279835aa3a4ffe9625
loudnl <LouDnl@users.noreply.github.com>
2026-06-06 16:38:57 +0200

    main.clj

---
+++ src/usbsid/ui/main.clj
@@ -0,0 +1,333 @@
+(ns usbsid.ui.main
+  "Head view, on top, big boss"
+  (:require
+   [cljfx.api :as fx]
+   [clojure.java.io :as io]
+   [clojure.string :as string]
+   [usbsid.state :refer [log!]]
+   [usbsid.ui.css :as css]
+   [usbsid.ui.sections.sockets :as s-sockets]
+   [usbsid.ui.sections.clock :as s-clock]
+   [usbsid.ui.sections.leds :as s-leds]
+   [usbsid.ui.sections.features :as s-features]
+   [usbsid.ui.sections.sid-tests :as s-tests]
+   [usbsid.ui.sections.debug :as s-debug]
+   [usbsid.ui.sections.about :as s-about]
+   [usbsid.window-prefs :as w-prefs])
+  (:import
+   [javafx.application Platform]
+   [javafx.beans.value ChangeListener]
+   [javafx.scene.image Image]))
+
+
+;;; State startup/shutdown refs
+
+(def signalled (promise))
+(def stopped   (promise))
+
+
+;;; Here be stuffs
+
+(def nav-items
+  [{:key :about     :label "ABOUT"}
+   {:key :sockets   :label "SOCKETS"}
+   {:key :clock     :label "CLOCK"}
+   {:key :leds      :label "LEDS"}
+   {:key :features  :label "FEATURES"}
+   {:key :sid-tests :label "SID TESTS"}
+   {:key :debug     :label "DEBUG"}])
+
+(defn- logo-image
+  "Well what do you think it is?"
+  []
+  (try (Image. (str (io/resource "usbsid-configtool-logo.png")))
+       (catch Exception _ nil)))
+
+(defn- icon-image
+  "It's so smol!"
+  []
+  (try (Image. (str (io/resource "usbsid-configtool-icon.png")))
+       (catch Exception _ nil)))
+
+
+;;; Here be what you see
+
+(defn title-bar
+  "Title shmitle, who cares!?"
+  [{:keys [connection]}]
+  (let [connected? (= :connected (:status connection))
+        cfgstatus  (:config-status connection)
+        fw-ver     (:fw-version connection)
+        pcb-ver    (:pcb-version connection)]
+    {:fx/type     :border-pane
+     :style-class "c64-title-bar"
+     :left
+     {:fx/type  :h-box
+      :spacing  10
+      :alignment :center-left
+      :children
+      [(if-let [img (logo-image)]
+         {:fx/type      :image-view
+          :image        img
+          :fit-height   40
+          :preserve-ratio true}
+         {:fx/type :pane})
+       {:fx/type  :v-box
+        :spacing  2
+        :children
+        [{:fx/type     :label
+          :text        " USBSID-Pico Configuration Tool" ; ISSUE: This is not neatly ligned out yet
+          :style-class "c64-title-text"}
+         {:fx/type     :label
+          :text        (format "  v%s | %s | %s"
+                               (string/trim-newline (slurp (io/resource ".version")))
+                               (if fw-ver (str "FW: v" fw-ver) "FW: ---")
+                               (if pcb-ver (str "PCB: v" pcb-ver) "PCB: ---"))
+          :style-class "c64-subtitle-text"}
+         {:fx/type     :label
+          :alignment   :center-left
+          :text        (case cfgstatus
+                         :loaded    "  Config loaded!"
+                         :confirmed "  Config confirmed!"
+                         :written   "  Config written!"
+                         :saved     "  Config saved!"
+                         :exportini "  Config exported to ini!"
+                         :importini "  Config imported from ini!"
+                         :applied   "  Config applied!"
+                         "")
+          :style-class (case cfgstatus
+                         :loaded    "c64-config-loaded"
+                         :confirmed "c64-config-confirmed"
+                         :written   "c64-config-written"
+                         :saved     "c64-config-saved"
+                         :exportini "c64-config-exported"
+                         :importini "c64-config-imported"
+                         :applied   "c64-config-applied"
+                         "c64-subtitle-text")}]}]}
+     :right
+     {:fx/type    :v-box
+      :spacing    4
+      :alignment  :center-right
+      :padding    {:top 4 :right 0 :bottom 4 :left 0}
+      :children
+      [{:fx/type     :label
+        :alignment   :center
+        :max-width   Double/MAX_VALUE
+        :text        (if connected? "** CONNECTED **" "** DISCONNECTED **")
+        :style-class (if connected? "c64-status-connected" "c64-status-disconnected")}
+       {:fx/type   :h-box
+        :spacing   6
+        :children
+        [{:fx/type     :button
+          :text        "CONNECT"
+          :on-action   {:event/type :connect}
+          :disable     connected?
+          :style-class (if connected? "c64-button" "c64-button-primary")}
+         {:fx/type     :button
+          :text        "DISCONNECT"
+          :on-action   {:event/type :disconnect}
+          :disable     (not connected?)
+          :style-class (if connected? "c64-button-primary" "c64-button")}]}]}}))
+
+(defn warning-banner
+  "Well that's what you get for messing with me!"
+  []
+  {:fx/type     :h-box
+   :style-class "c64-warning-banner"
+   :spacing     12
+   :padding     {:top 6 :right 8 :bottom 6 :left 8}
+   :children
+   [{:fx/type     :label
+     :text        "!!! CONFIGURATION NEEDS VERIFICATION - SOCKETS DISABLED UNTIL CONFIRMED !!!"
+     :style-class "c64-warning-text"}
+    {:fx/type     :button
+     :text        "CONFIRM"
+     :on-action   {:event/type :confirm-config}
+     :style-class "c64-button-danger"}]})
+
+(defn nav-panel
+  "We navigate the stars together!"
+  [{:keys [active-section]}]
+  {:fx/type     :v-box
+   :style-class "c64-nav"
+   :spacing     0
+   :children
+   (mapv (fn [{:keys [key label]}]
+           {:fx/type     :button
+            :text        label
+            :on-action   {:event/type :navigate :section key}
+            :style-class (if (= key active-section)
+                           ["c64-nav-item" "c64-nav-item-active"]
+                           "c64-nav-item")})
+         nav-items)})
+
+(defn content-panel
+  "Look at me, I can see you!"
+  [{:keys [active-section config connected? connection]}]
+  {:fx/type      :scroll-pane
+   :style-class  "c64-scroll-pane"
+   :fit-to-width true
+   :hbar-policy  :never
+   :h-box/hgrow  :always
+   :content
+   (case active-section
+     :sockets   (s-sockets/sockets-section {:config config})
+     :clock     (s-clock/clock-section {:config config})
+     :leds      (s-leds/leds-section {:config config})
+     :features  (s-features/features-section {:config config})
+     :sid-tests (s-tests/sid-tests-section {:connected? connected?})
+     :debug     (s-debug/debug-section {:connected? connected?})
+     :about     (s-about/about-section {:connection connection})
+     {:fx/type     :label
+      :text        "Select a section"
+      :style-class "c64-label"})})
+
+(defn action-bar
+  "Are you getting any!?"
+  [{:keys [connected?]}]
+  {:fx/type  :v-box
+   :spacing  0
+   :children
+   [{:fx/type     :h-box
+     :style-class "c64-hbox"
+     :padding     {:top 4 :right 8 :bottom 2 :left 8}
+     :spacing     6
+     :children
+     [{:fx/type     :button
+       :text        "READ CONFIG"
+       :on-action   {:event/type :read-config}
+       :disable     (not connected?)
+       :wrap-text   true
+       :style-class "c64-button"}
+      {:fx/type     :button
+       :text        "WRITE CONFIG"
+       :on-action   {:event/type :write-config}
+       :disable     (not connected?)
+       :wrap-text   true
+       :style-class "c64-button"}
+      {:fx/type     :button
+       :text        "APPLY CFG"
+       :on-action   {:event/type :apply-config}
+       :disable     (not connected?)
+       :wrap-text   true
+       :style-class "c64-button"}
+      {:fx/type     :pane
+       :h-box/hgrow :always}
+      {:fx/type     :button
+       :text        "SAVE (NO REBOOT)"
+       :on-action   {:event/type :save-noreset}
+       :disable     (not connected?)
+       :wrap-text   true
+       :style-class "c64-button"}
+      {:fx/type     :button
+       :text        "SAVE + REBOOT"
+       :on-action   {:event/type :save-config}
+       :disable     (not connected?)
+       :wrap-text   true
+       :style-class "c64-button-primary"}
+      {:fx/type     :button
+       :text        "RELOAD FLASH"
+       :on-action   {:event/type :reload-flash}
+       :disable     (not connected?)
+       :wrap-text   true
+       :style-class "c64-button"}]}
+    {:fx/type     :h-box
+     :style-class "c64-hbox"
+     :padding     {:top 2 :right 8 :bottom 4 :left 8}
+     :spacing     6
+     :children
+     [{:fx/type     :button
+       :text        "SAVE INI"
+       :on-action   {:event/type :export-ini}
+       :style-class "c64-button"}
+      {:fx/type     :button
+       :text        "LOAD INI"
+       :on-action   {:event/type :import-ini}
+       :style-class "c64-button"}
+      {:fx/type     :pane
+       :h-box/hgrow :always}
+      {:fx/type     :button
+       :text        "RESET DEFAULTS"
+       :on-action   {:event/type :reset-config}
+       :disable     (not connected?)
+       :style-class "c64-button-danger"}]}]})
+
+(defn- log-text-area
+  "Here be letters!"
+  [{:keys [log]}]
+  {:fx/type    fx/ext-on-instance-lifecycle
+   :on-created (fn [^javafx.scene.control.TextArea ta]
+                 (.addListener
+                  (.textProperty ta)
+                  (reify ChangeListener
+                    (changed [_ _ _ new-text]
+                      (Platform/runLater
+                       (fn [] (.positionCaret ta (count new-text))))))))
+   :desc       {:fx/type     :text-area
+                :style-class "c64-log-area"
+                :text        (string/join "\n" log)
+                :editable    false
+                :wrap-text   false
+                :pref-height 100
+                :min-height  80}})
+
+(defn log-panel
+  "It's where the leprechauns hide"
+  [{:keys [log]}]
+  {:fx/type  :v-box
+   :spacing  0
+   :children
+   [{:fx/type     :label
+     :text        " LOG:"
+     :style-class "c64-label-dim"}
+    (log-text-area {:log log})]})
+
+(defn root-view
+  "In your face!"
+  [{:keys [connection config active-section log]
+    :as _state}]
+  (let [connected?    (= :connected (:status connection))
+        need-confirm? (get config :need-confirmation false)]
+    {:fx/type  :stage
+     :showing  true
+     :title    "USBSID-Pico Config Tool"
+     :on-close-request (fn [_]
+                         #_(.fine @logging/logger "> Application is set to close")
+                         (log! "Application is set to close")
+                         (w-prefs/set-window-state)
+                         (deliver signalled true))
+     :on-hidden (fn [_]
+                  #_(.fine @logging/logger "> Application closed")
+                  (log! "Application closed")
+                  (w-prefs/set-window-state)
+                  (deliver stopped true))
+     ;;  :min-width  900
+     ;;  :min-height 650
+     ;;  :width      1280
+     ;;  :height     1024
+     :min-width  1024
+     :min-height 768
+     :icons (let [img (icon-image)] (if img [img] []))
+     :scene
+     {:fx/type     :scene
+      :stylesheets css/stylesheets
+      :root
+      {:fx/type     :border-pane
+       :style-class "c64-scene-root"
+       :top
+       {:fx/type  :v-box
+        :spacing  0
+        :children (cond-> [(title-bar {:connection connection})]
+                    need-confirm? (conj (warning-banner)))}
+       :center
+       {:fx/type  :h-box
+        :children [(nav-panel {:active-section active-section})
+                   (content-panel {:active-section active-section
+                                   :config         config
+                                   :connected?     connected?
+                                   :connection     connection})]}
+       :bottom
+       {:fx/type  :v-box
+        :spacing  0
+        :children [(action-bar {:connected? connected?})
+                   (log-panel {:log log})]}}}}))
