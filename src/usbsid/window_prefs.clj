e72efa6d221d4c398eb1d4abf76969bd7ec08463
loudnl <LouDnl@users.noreply.github.com>
2026-06-06 16:12:57 +0200

    window_prefs.clj

---
+++ src/usbsid/window_prefs.clj
@@ -0,0 +1,45 @@
+(ns usbsid.window-prefs
+  "Save Window size an location, because I can :P"
+  (:import
+   [java.util.prefs Preferences]
+   [javafx.stage Window]))
+
+
+; This is, well if you use Linux, located at ~/.java/.userPrefs/usbsid-pico-configtool/prefs.xml
+(def ^:private ^Preferences node
+  (.node (Preferences/userRoot) "usbsid-pico-configtool"))
+
+(defn load-window
+  "Load window settings from node"
+  []
+  {:x (.getDouble node "x" Double/NaN)
+   :y (.getDouble node "y" Double/NaN)
+   :w (.getDouble node "w" 1280.0)
+   :h (.getDouble node "h" 1024.0)})
+
+(defn save-window!
+  "Save window settings to node"
+  [{:keys [x y w h]}]
+  (.putDouble node "x" x)
+  (.putDouble node "y" y)
+  (.putDouble node "w" w)
+  (.putDouble node "h" h)
+  (try (.flush node) (catch Exception _)))
+
+(defn set-window-state
+  "Set the window state"
+  []
+  (let [{:keys [x y w h]} (load-window)]
+    (when-let [^Window win (first (Window/getWindows))]
+      (.setWidth  win w)
+      (.setHeight win h)
+      (when-not (Double/isNaN x) (.setX win x))
+      (when-not (Double/isNaN y) (.setY win y)))))
+
+(defn save-window-state
+  "Save the window state"
+  []
+  (when-let [^Window win (first (Window/getWindows))]
+    (save-window!
+     {:x (.getX win) :y (.getY win)
+      :w (.getWidth win) :h (.getHeight win)})))
