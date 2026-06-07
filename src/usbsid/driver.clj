85c5b96d5dcafef4c4873c47e96e614e6cce8023
loudnl <LouDnl@users.noreply.github.com>
2026-06-07 22:28:45 +0200

    Winblows testing!

--- src/usbsid/driver.clj
+++ src/usbsid/driver.clj
@@ -258,10 +258,9 @@
     (fn []
       (reset! drv-atom (USBSID.))
       (state/log! "Connecting to USBSID-Pico")
-      (let [windows? (USBSIDDevice/isWinblows)
-            result   (if windows?
-                       (.USBSID_init @drv-atom "libusb-winusb" (int 8192) (int 64))
-                       (.USBSID_init @drv-atom))]
+      (let [result (if (USBSIDDevice/isWinblows)
+                     (.USBSID_init @drv-atom "libusb-winusb" (int 8192) (int 64))
+                     (.USBSID_init @drv-atom))]
         (if (zero? result)
           (let [fw  (read-with-retry #(do-read-fwversion) valid-fw-version? 3 50)
                 pcb (read-with-retry #(do-read-pcbversion) valid-pcb-version? 3 50)]
