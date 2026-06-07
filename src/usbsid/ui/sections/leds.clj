dca13b5ad6e4211cc43c8c943f29be2d6550731f
loudnl <LouDnl@users.noreply.github.com>
2026-06-07 16:22:57 +0200

    Well duh!

--- src/usbsid/ui/sections/leds.clj
+++ src/usbsid/ui/sections/leds.clj
@@ -96,7 +96,7 @@
                                             (state/set-config-value!
                                              [:rgbled :sid-to-use] (:key s))))})
 
-          (w/c64-labeled-slider (format "%-24s" "Brightness") 0 255 (:brightness rgb)
+          (w/c64-labeled-slider (format "%-20s" "Brightness") 0 255 (:brightness rgb)
                                 (fn [v]
                                   (state/set-config-value!
                                    [:rgbled :brightness] (int v)))
