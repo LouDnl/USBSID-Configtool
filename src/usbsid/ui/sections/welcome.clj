1b1b90dc6f6b7a82a75b953425d74079da41af7f
loudnl <LouDnl@users.noreply.github.com>
2026-06-13 14:07:09 +0200

    about -> welcome

--- src/usbsid/ui/sections/welcome.clj
+++ src/usbsid/ui/sections/welcome.clj
@@ -1,18 +1,26 @@
-(ns usbsid.ui.sections.about
+(ns usbsid.ui.sections.welcome
   "So, what's it all about then?!"
   (:require
+   [usbsid.config-model :refer [parse-fw-version]]
    [usbsid.ui.sections.common :as common]
    [usbsid.ui.widgets :as w]))
 
 (defn about-section
   "Generates the about tab, pagina uno"
   [{:keys [connection]}]
-  (let [fw  (get-in connection [:fw-version])
-        pcb (get-in connection [:pcb-version])]
+  (let [fw       (get-in connection [:fw-version])
+        pcb      (get-in connection [:pcb-version])
+        fwverint (parse-fw-version fw)
+        warningsection (if (>= fwverint 70)
+                         common/warning-text-block
+                         common/warning-text-block-legacy)]
     {:fx/type     :v-box
      :style-class "c64-vbox"
      :spacing     10
-     :padding     {:top 8 :right 8 :bottom 8 :left 8}
+     :padding     {:top    8
+                   :right  8
+                   :bottom 8
+                   :left   8}
      :children
      [common/welcome-text-block
 
@@ -20,7 +28,7 @@
       common/info-text-block
 
       (w/c64-separator)
-      common/warning-text-block
+      warningsection
 
       (w/c64-separator)
       common/about-text-block
